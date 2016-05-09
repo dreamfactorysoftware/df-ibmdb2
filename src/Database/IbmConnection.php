7c787ff3b7ff910a51c3a791ff466ff55203f734
Lee Hicks <leehicks@dreamfactory.com>
2016-05-08 22:28:13 -0400

    initial load

---
+++ src/Database/IbmConnection.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace DreamFactory\Core\IbmDb2\Database;
+
+use Illuminate\Database\Connection;
+
+class IbmConnection extends Connection
+{
+}
---
+++ src/Database/Schema/ColumnSchema.php
@@ -0,0 +1,10 @@
+<?php
+namespace DreamFactory\Core\IbmDb2\Database\Schema;
+
+/**
+ * ColumnSchema class describes the column meta data of a DB2 table.
+ */
+class ColumnSchema extends \DreamFactory\Core\Database\Schema\ColumnSchema
+{
+
+}
