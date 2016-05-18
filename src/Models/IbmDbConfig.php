7bca90d3ce20cf2fb43b491e3913f75f1fcbc759
Lee Hicks <leehicks@dreamfactory.com>
2016-05-17 22:47:08 -0400

    updating format of config saved to database

--- src/Models/IbmDbConfig.php
+++ src/Models/IbmDbConfig.php
@@ -14,12 +14,6 @@ class IbmDbConfig extends SqlDbConfig
         return 'ibm';
     }
 
-    public static function getDefaultDsn()
-    {
-        // http://php.net/manual/en/ref.pdo-ibm.connection.php
-        return 'ibm:DRIVER={IBM DB2 ODBC DRIVER};DATABASE=db;HOSTNAME=localhost;PORT=56789;PROTOCOL=TCPIP;';
-    }
-
     public static function getDefaultPort()
     {
         return 56789;
