1e13c12c0eea7d14bff3cdeef4a6cfc0f4ba8941
Lee Hicks <leehicks@dreamfactory.com>
2017-10-31 16:34:30 -0400

    Do not set schema on connection, handled later

--- src/Database/Connectors/IbmConnector.php
+++ src/Database/Connectors/IbmConnector.php
@@ -10,29 +10,22 @@ class IbmConnector extends Connector implements ConnectorInterface
     /**
      * Establish a database connection.
      *
-     * @param  array  $config
+     * @param  array $config
      * @return \PDO
+     * @throws \Exception
      */
     public function connect(array $config)
     {
         $dsn = $this->getDsn($config);
-
         $options = $this->getOptions($config);
-        $connection = $this->createConnection($dsn, $config, $options);
 
-        if (isset($config['schema'])) {
-            $schema = $config['schema'];
-
-            $connection->prepare("set schema $schema")->execute();
-        }
-
-        return $connection;
+        return $this->createConnection($dsn, $config, $options);
     }
 
     /**
      * Create a DSN string from a configuration.
      *
-     * @param  array   $config
+     * @param  array $config
      * @return string
      */
     protected function getDsn(array $config)
@@ -40,21 +33,21 @@ class IbmConnector extends Connector implements ConnectorInterface
         extract($config, EXTR_SKIP);
 
         $dsn = "ibm:";
-        
-        if (empty($driverName)){
+
+        if (empty($driverName)) {
             $driverName = "{IBM DB2 ODBC DRIVER}";
         }
         $dsn .= "DRIVER={$driverName};";
-        if (!empty($host)){
+        if (!empty($host)) {
             $dsn .= "HOSTNAME={$host};";
         }
-        if (!empty($port)){
+        if (!empty($port)) {
             $dsn .= "PORT={$port};";
         }
-        if (!empty($protocol)){
+        if (!empty($protocol)) {
             $dsn .= "PROTOCOL={$protocol};";
         }
-        if (!empty($database)){
+        if (!empty($database)) {
             $dsn .= "DATABASE={$database};";
         }
 
